b71d0c4ed73e9d935d4bd94948d32414484ae94c
神奇小元宵 <43571851+aqbxfe@users.noreply.github.com>
2019-05-29 22:46:32 +0800

    删除测试
    把测试类删除

--- src/main/java/com/feng/demo/test1/Test.java
+++
@@ -1,7 +0,0 @@
-package com.feng.demo.test1;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hollo World");
-    }
-}
--- src/main/java/com/feng/demo/test1/Test2.java
+++
@@ -1,4 +0,0 @@
-package com.feng.demo.test1;
-
-public class Test2 {
-}
